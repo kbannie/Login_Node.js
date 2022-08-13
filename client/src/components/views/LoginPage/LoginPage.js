9860fae46395e0c657410e2169c84c60868c2c24
kabeen <kabin2205@gamil.com>
2022-08-13 18:44:24 +0900

    다른 오류 발생

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function LoginPage() {
   return (
-    <div>
-      LoginPage
+    <div style={{
+      display:'flex', justifyContent:'content',alignItems:'center',
+      width:'100%',height:'100vh'
+    }}>
+      <h2>시작 페이지</h2>
     </div>
   )
 }
